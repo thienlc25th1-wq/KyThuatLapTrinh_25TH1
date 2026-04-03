207e4ff3cdf0d1fe4aa4ee6325e9994f81a32ee4
PMT201 <PMT201@PMT3030>
2026-04-03 10:36:23 +0700

    buoi5

--- KyThuatLapTrinh/Buoi1/kythuatlaptrinh/buoi5/buoi5.cpp
+++ KyThuatLapTrinh/Buoi1/kythuatlaptrinh/buoi5/buoi5.cpp
@@ -10,18 +10,85 @@ struct Book {
     int id;
     string name;
     Athor author;
+    friend ostream& operator << (ostream& os, const Book& b) {
+        os << "Book information: " << endl;
+        os << "\t+ Id: " << b.id << endl;
+        os << "\t+ Name: " << b.name << endl;
+        os << "\t+ Author name: " << b.author.name << endl;
+        return os;
+    }
 };
 struct Node {
     Book data;
-    Node* next
+    Node* next;
 };
 struct LinkedList {
     Node* head;
 
 };
+void Show(LinkedList books) {
+    if (books.head == NULL) {
+        cout << "No book available" << endl;
+        return;
+    }
+    Node* item = books.head;
+    while (item != NULL) {
+        cout << item->data;
+        item = item->next;
+    }
+}
 
 int main()
 {
     LinkedList books = { NULL };
+    do {
+        system("cls");
+        cout << "--------BOOK MANAGEMENT---------" << endl;
+        cout << "1. show all books " << endl;
+        cout << "2. Add a book" << endl;
+        cout << "3.Delete a book" << endl;
+        cout << "4. Update a book" << endl;
+        cout << "5. Find book" << endl;
+        cout << "6. Export to file" << endl;
+        cout << "7. Import from file" << endl;
+        cout << "0. Exit" << endl;
+        cout << "------------------------------" << endl;
+        cout << "Enter your choice: ";
+        int choice;
+        cin >> choice;
+        switch (choice)
+        {
+        case 1: {
+            Show(books);
+            break;
+        }
+        case 2: {
+            break;
+        }
+          case3: {
+              break;
+        }
+        case 4: {
+            break;
+        }
+        case 5: {
+            break;
+        }
+        case 6: {
+            break;
+        }
+        case 7: {
+            break;
+        }
+        case 0: {
+            break;
+        }
+        default: {
+            cout << "Inalid choice, try again" << endl;
+            break;
+        }
+
+        }
+    } while (true);
 }
 
